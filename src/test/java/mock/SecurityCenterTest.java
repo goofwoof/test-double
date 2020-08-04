9e963c4c1a7ac7efa7def42bd38bbb5d196a032a
goofwoof <heli0086@outlook.com>
2020-08-04 10:34:07 +0800

    Mock test submit

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -8,12 +8,15 @@ public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
-
+    SecurityCenter sc;
     @BeforeEach
     public void setUp() {
+        DoorPanel dp = new MockDoorPanel();
+         sc = new SecurityCenter(dp);
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+        sc.switchOn();
     }
 }
